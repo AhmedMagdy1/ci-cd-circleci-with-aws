bbc335375ed06e62463ac20ffa4a4f0585d1c4c8
ahmed magdy <amagdy@tendegrees.sa>
2022-09-28 11:49:10 +0200

    update config

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://final11bucket/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://final11bucket/
+aws s3 cp --recursive --acl public-read ./www s3://find12bucket/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://find12bucket/
